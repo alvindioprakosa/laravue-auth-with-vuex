d1d0afd5903b8cc4f006ee29c0fff82860cc8811
alvindioprakosa <alvindioprakosa@gmail.com>
2025-02-05 16:49:40 +0700

    Repair store.js

--- backup/store.js
+++ backup/store.js
@@ -1,33 +1,33 @@
-
-
-import Vuex from 'vuex'
-import subject from './subject'
+import Vuex from "vuex";
+import subject from "./subject";
 
 export default new Vuex.Store({
     state: {
         subjects: [],
     },
     mutations: {
-        subject(state,data) {
-            return state.subjects = data
-         }
+        subject(state, data) {
+            return (state.subjects = data);
+        },
     },
     actions: {
-        allCategoryFromDatabase(context){
-            axios.get("api/subject")
-               .then((response)=>{
-                  context.commit("subject",response.data.data) 
-               }).catch(()=>{ 
-                  console.log("Error") 
-               })
-        }
+        allCategoryFromDatabase(context) {
+            axios
+                .get("api/subject")
+                .then((response) => {
+                    context.commit("subject", response.data.data);
+                })
+                .catch(() => {
+                    console.log("Error");
+                });
+        },
     },
     getters: {
-        getSubject(state){ 
-            return state.subjects
-         }
+        getSubject(state) {
+            return state.subjects;
+        },
     },
     modules: {
-        subject
-    }
-  })
+        subject,
+    },
+});
