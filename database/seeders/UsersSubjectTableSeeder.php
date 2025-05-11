5f53d8b6f1a1d10ba5aeaff2a611587317b1b232
alvindioprakosa <alvindioprakosa@gmail.com>
2025-05-11 23:04:52 +0700

    Update Users Subject Table Seeder

--- database/seeders/UsersSubjectTableSeeder.php
+++ database/seeders/UsersSubjectTableSeeder.php
@@ -3,20 +3,17 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
-use Illuminate\Support\str;
 use App\Models\UserSubject;
 
 class UsersSubjectTableSeeder extends Seeder
 {
     /**
      * Run the database seeds.
-     *
-     * @return void
      */
-    public function run()
+    public function run(): void
     {
-        UserSubject::Create([
-            'user_id' => 1, 
+        UserSubject::create([
+            'user_id' => 1,
             'subject_id' => 1,
         ]);
     }
