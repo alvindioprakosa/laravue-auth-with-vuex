d56c2b33b562bfc320070d1ac1f7ae8755e8c1f7
alvindioprakosa <alvindioprakosa@gmail.com>
2025-05-11 23:04:17 +0700

    Update Data Seder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -8,13 +8,13 @@ class DatabaseSeeder extends Seeder
 {
     /**
      * Seed the application's database.
-     *
-     * @return void
      */
-    public function run()
+    public function run(): void
     {
-        $this->call(UsersTableSeeder::class);
-        $this->call(SubjectTableSeeder::class);
-        $this->call(UsersSubjectTableSeeder::class);
+        $this->call([
+            UsersTableSeeder::class,
+            SubjectTableSeeder::class,
+            UsersSubjectTableSeeder::class,
+        ]);
     }
 }
