50c44c47176969d77fba3fe4872e7cc1def789ce
alvindioprakosa <alvindioprakosa@gmail.com>
2025-05-11 23:04:02 +0700

    Update View

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,15 +1,20 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
   <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="csrf-token" content="{{csrf_token()}}">
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta name="csrf-token" content="{{ csrf_token() }}" />
+
+    <title>Vue Router App</title>
+
+    <!-- Tailwind CSS CDN (untuk dev; untuk produksi disarankan build dengan PostCSS) -->
     <script src="https://cdn.tailwindcss.com"></script>
-    <title>Vue-Router</title>  
   </head>
   <body>
     <div id="app"></div>
+
+    <!-- Laravel Mix asset -->
     <script src="{{ mix('js/app.js') }}"></script>
   </body>
 </html>
